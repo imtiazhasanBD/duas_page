26001168e7c43f7f10a699ca44c5dc4b98a612f7
imtiazhasanBD <imtiazhasan500@gmail.com>
2025-01-19 03:26:07 +0600

    add metadata and favicon

--- client/app/layout.js
+++ client/app/layout.js
@@ -7,6 +7,12 @@ import SkeletonCategory from "./components/SkeletonCategory";
 import SettingSection from "./components/SettingSection";
 import CustomLayout from "./components/CustomLayout";
 
+export const metadata = {
+  title: 'Dua & Ruqyah | All Duas Collection',
+  description: 'Explore a comprehensive collection of duas and ruqyah categorized for easy navigation. Discover, search, and access Arabic text, transliteration, translation, and audio for each dua in a modern and user-friendly platform.',
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
